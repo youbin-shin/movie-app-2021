c79c8c1332dc0047817c47476790659f9e4c6754
youbin-shin <youbin5575@gmail.com>
2021-02-27 21:18:24 +0900

    new component add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+function Youbin(props) {
+  return (
+      <div>
+          <h3>I like { props.fav }. 정도는 {props.degree}</h3>
+      </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <Youbin fav="kickek" degree="so much"/>
+      <Youbin fav="ramen" degree="medium"/>
     </div>
   );
 }
