277799f8cd9c1b2ba7a02f4a1e411f727abe7c54
youbin-shin <youbin5575@gmail.com>
2021-02-28 23:34:48 +0900

    map recap

--- src/App.js
+++ src/App.js
@@ -10,27 +10,32 @@ function Youbin({name, degree}) {
 
 const foods = [
   {
+    id: 1, 
     name: "a",
     degree: "1"
   },
   {
+    id: 2,
     name: "b", 
     degree: "2"
   },
   {
+    id: 3, 
     name: "c",
     degree: "3"
   },
   {
+    id: 4,
     name: "d",
     degree: "4"
   }
 ]
 
+
 function App() {
   return (
     <div>
-      {foods.map(food => <Youbin name={food.name} degree={food.degree}/>)}
+      {foods.map((food) => <Youbin key={food.id} name={food.name} degree={food.degree}/> )}
     </div>
   );
 }
