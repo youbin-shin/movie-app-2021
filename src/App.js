ceba679c368304d4d165d2b95594cdd03eee71b5
youbin-shin <youbin5575@gmail.com>
2021-03-01 17:27:33 +0900

    movies component에 movie data rendering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
   state = {
@@ -7,16 +8,28 @@ class App extends React.Component {
     movies: []
   };
   getMovies = async () => {
-    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
     this.setState({ movies, isLoading: false })
   };
   componentDidMount() {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state
+    const { isLoading, movies } = this.state
     return (
-      <div>{isLoading? "Loading..." : "Ready now"}</div>
+      <div>
+        {isLoading? "Loading..." : movies.map(movie  => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year} 
+            title={movie.title} 
+            summary={movie.summary} 
+            poster={movie.medium_cover_image} 
+            />
+        )
+        )}
+      </div>
     )
   }
 }
