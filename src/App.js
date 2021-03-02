a85375aa57bb21f29e3ad2b00edc0e5236a6367e
youbin-shin <youbin5575@gmail.com>
2021-03-03 00:28:37 +0900

    modify convention

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     movies: []
   };
   getMovies = async () => {
-    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
     this.setState({ movies, isLoading: false })
   };
   componentDidMount() {
@@ -18,29 +18,31 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state
     return (
-      <section class="container">
-        {isLoading ? 
-          (<div class="loader">
-              <span class="loader__text">Loading...</span>
-            </div>
+      <section className="container">
+        {isLoading ?
+          (<div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
           ) : (
-            <div class="movies">
+            <div className="movies">
               {
-                movies.map(movie  => (
+                movies.map(movie => (
                   <Movie
                     key={movie.id}
                     id={movie.id}
-                    year={movie.year} 
-                    title={movie.title} 
-                    summary={movie.summary} 
-                    poster={movie.medium_cover_image} 
-                    />
+                    year={movie.year}
+                    title={movie.title}
+                    summary={movie.summary}
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres}
+                  />
                 ))
               }
             </div>
-             )}
+          )}
       </section>
-  )}
+    )
+  }
 }
 
 
