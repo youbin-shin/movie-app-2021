bfbd7f34a0fa1e10cbf7a6045e12271252df8da3
youbin-shin <youbin5575@gmail.com>
2021-03-03 12:23:47 +0900

    initial completion

--- src/Movie.js
+++ src/Movie.js
@@ -10,7 +10,7 @@ function Movie({ year, title, summary, poster, genres }) {
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
         <ul className="movie__genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}</ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   )
